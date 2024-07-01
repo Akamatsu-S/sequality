2be5553c5b795ef21074600f13b69e7f8f66305c
Akamatsu Shinichiro <e1n21002@oit.ac.jp>
2024-07-01 10:33:14 +0900

    testAppReternHelloWorld

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -8,8 +8,13 @@ import static org.junit.Assert.*;
 
 public class AppTest {
   @Test
-  public void appHasAGreeting() {
+  // public void appHasAGreeting() {
+  // App classUnderTest = new App();
+  // assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  // }
+
+  public void testAppReternHelloWorld() {
     App classUnderTest = new App();
-    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    assertEquals("Hello World!", classUnderTest.getGreeting());
   }
 }
